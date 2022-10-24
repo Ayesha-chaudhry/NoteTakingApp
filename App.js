3236ce896b118b35e0f68c1d1ee972d4b543c024
Ayesha-ch <66402334+Ayesha-chaudhry@users.noreply.github.com>
2022-10-24 16:47:55 +0600

    push Notification code

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {Text, View, StyleSheet} from 'react-native';
 import Navigation from "./src/navigation";
 import {AdMobBanner} from 'expo-ads-admob';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {  
+
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: false,
+        }),
+      });
+
+        useEffect(() => {
+            //registerForPushNotificationsAsync();
+        }, []);
     return(
         <View style={styles.container}>
             <Navigation/>
@@ -11,11 +24,28 @@ export default function App() {
                 bannerSize="smartBannerPortrait"
                 adUnitID="ca-app-pub-3940256099942544/6300978111" 
                 servePersonalizedAds 
-            />
-                    
+            />                    
         </View>   
-
   );
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+
+    return token;
+}
       
 };
 
